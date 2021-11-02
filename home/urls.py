c1c2a27ab51cf82b30f083230ec6a43e195e70c2
Gurol Bilgin <gurolbilgin@gmail.com>
2021-11-01 20:01:51 -0600

    adds the changes up and running

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import about, home, teacher
 
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('teacher/', teacher, name='teacher'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home/index.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
+
+def teacher(request):
+    return render(request, 'home/teacher.html')
